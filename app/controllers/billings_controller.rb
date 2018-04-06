34818fa5a21c7ff61d0d1f77140d9d818d6666f6
Felipe <f.mahana94@gmail.com>
2018-04-06 16:40:43 -0300

    PayPal casi funcional

--- app/controllers/billings_controller.rb
+++ app/controllers/billings_controller.rb
@@ -39,7 +39,7 @@ class BillingsController < ApplicationController
 		  :payer =>  {
 		    :payment_method =>  "paypal" },
 		  :redirect_urls => {
-		    :return_url => "http://localhost:3000/payment/execute",
+		    :return_url => "http://localhost:3000/billings/execute",
 		    :cancel_url => "http://localhost:3000/" },
 		  :transactions =>  [{
 		    :item_list => {
@@ -53,15 +53,42 @@ class BillingsController < ApplicationController
 		      :currency =>  "USD" },
 		    :description =>  "This is the payment transaction description." }]})
 
-		render json: @payment.to_json
+		#render json: @payment.to_json
 
-		# if @payment.create
-		# 	render json: @payment.to_json
-		# 	#redirect_url = payment.links.find{|v| v.method == "REDIRECT" }.href
-		# 	#redirect_to redirect_url
-		# else
-		# 	render json: payment.error
-		# end
+		if @payment.create
+			#render json: @payment.to_json
+			redirect_url = @payment.links.find{|v| v.method == "REDIRECT" }.href
+			redirect_to redirect_url
+		else
+			#':c'
+			render json: payment.error
+		end
 
+	end
+
+	def execute
+		paypal_payment = PayPal::SDK::REST::Payment.find(params[:paymentId])
+
+		if paypal_payment.execute(payer_id: params[:PayerID])
+			amount = paypal_payment.transactions.first.amount.total
+
+			billing = Billing.create(
+				user: current_user,
+				code: paypal_payment.id,
+				payment_method: 'paypal',
+				amount: amount,
+				currency: 'USD'
+				)
+
+			#orders = current_user.orders.where(payed: false)
+			#orders.update_all(payed: true, billing_id: billing.id)
+
+			redirect_to root_path, notice: "El pago se realizó con éxito!" 
+		  # Success Message
+		  # Note that you'll need to `Payment.find` the payment again to access user info like shipping address
+		else
+		  render plain: "No se puedo generar el cobro en PayPal."
+		  paypal_payment.error # Error Hash
+		end
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180405222724) do
+ActiveRecord::Schema.define(version: 20180406142847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20180405222724) do
     t.string "currency"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_billings_on_user_id"
   end
 
   create_table "contracts", force: :cascade do |t|
@@ -89,6 +91,7 @@ ActiveRecord::Schema.define(version: 20180405222724) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "billings", "users"
   add_foreign_key "contracts", "billings"
   add_foreign_key "contracts", "plans"
   add_foreign_key "contracts", "users"
